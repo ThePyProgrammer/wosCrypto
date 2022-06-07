6a70ceb3a0efc6440412cc2609fe9c5ba9bb0eb4
Prannaya Gupta <50899509+ThePyProgrammer@users.noreply.github.com>
2022-06-07 18:31:13 +0800

    Create modulo.py

---
+++ modulo.py
@@ -0,0 +1,15 @@
+from math import floor, ceil, log
+
+class FastModulo:
+    @staticmethod
+    def exponentCalculate(base, exp, mod):
+        val = 1
+        for i in range(exp):
+            val = val * base % mod
+        return val
+
+    @staticmethod
+    def superExponentCalculate(base, exp, superexp, mod):
+        for i in range(superexp):
+            base = FastModulo.exponentCalculate(base, exp)
+        return base
